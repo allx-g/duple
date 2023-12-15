e1a24536600e7a560ed740a43c41abaf53c59ab4
Alex <alexg125125@gmail.com>
2023-12-14 22:18:48 -0600

    Add Trie DS for word storage
    When a player plays a round, all their words must be stored. To
    implement this efficiently, a Trie was used.

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const inputBox = document.querySelector("#user-input");
 const enterButton = document.querySelector("#submit-button");
 const messageDisplay = document.querySelector("#message-display");
 
-enterButton.addEventListener('click', manageInput);
+enterButton.addEventListener('click', handleInput);
 
-function manageInput() {
+function handleInput() {
     const input = inputBox.value.toLowerCase();
     clearInputField();
     
@@ -34,3 +34,61 @@ function manageInput() {
         messageDisplay.textContent = message;
     }
 }
+
+
+// Trie data structure implementation
+function TrieNode(key) {
+    this.key = key;
+    this.parent = null;
+    this.children = {}
+    this.end = false;
+    this.getWord = function() {
+        let output = [];
+        let node = this;
+
+        while (node !== null) {
+            output.unshift(node.key);
+            node = node.parent;
+        }
+
+        return output.join('');
+    }
+}
+
+function Trie() {
+    this.root = new TrieNode(null);
+
+    this.insert = function(word) {
+        let node = this.root;
+
+        for (let i = 0; i < word.length; i++) {
+
+            if (!node.children[word[i]]) {
+                node.children[word[i]] = new TrieNode(word[i]);
+                node.children[word[i]].parent = node;
+            }
+
+            node = node.children[word[i]];
+
+            if (i == word.length - 1) {
+                node.end = true;
+            }
+        }
+    }
+
+    this.contains = function(word) {
+        let node = this.root;
+
+        for (let i = 0; i < word.length; i++) {
+            if (node.children[word[i]]) {
+                node = node.children[word[i]];
+            }
+            else {
+                return false;
+            }
+        }
+
+        return node.end;
+    }
+}
+
